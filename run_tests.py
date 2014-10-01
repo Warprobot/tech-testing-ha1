200b1ad3ed44f5702dbdcd85af8b0ad1097566dd
Alexander Tsvetkov <ff.warprobot@gmail.com>
2014-10-01 18:53:35 +0400

    Seminar

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
+import contextlib
 
 import os
+import socket
 import sys
 import unittest
 
@@ -11,10 +13,26 @@ from tests.test_notification_pusher import NotificationPusherTestCase
 from tests.test_redirect_checker import RedirectCheckerTestCase
 
 
+def _create_connection(*args, **kwargs):
+    raise AssertionError('Unmocked http request')
+
+
+socket.create_connection = _create_connection
+
+
+@contextlib.contextmanager
+def mocked_connection():
+    original_connection = socket.create_connection
+    socket.create_connection = _create_connection
+    yield
+    socket.create_connection = original_connection
+
+
 if __name__ == '__main__':
     suite = unittest.TestSuite((
         unittest.makeSuite(NotificationPusherTestCase),
         unittest.makeSuite(RedirectCheckerTestCase),
     ))
-    result = unittest.TextTestRunner().run(suite)
+    with mocked_connection():
+        result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
--- source/tests/test_redirect_checker.py
+++ source/tests/test_redirect_checker.py
@@ -3,3 +3,7 @@ import unittest
 
 class RedirectCheckerTestCase(unittest.TestCase):
     pass
+    # def test_with_http_request(self):
+    #     import requests
+    #     r = requests.get('http://tech-mail.ru')
+    #     self.assertEquals(200, r.status_code, 'Bad response code')
